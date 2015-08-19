d7fff249f85c750b0d7789c309f3a74179ae25eb
Eric Katz <ericpkatz@gmail.com>
2015-08-19 23:42:29 +0000

    add jwt

--- routes/session.js
+++ routes/session.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var router = express.Router();
+var jwt = require("jwt-simple");
 
+router.get('/', function(req, res){
+    res.send(jwt.decode(req.query.token, process.env.SECRET || 'foo'));
+});
 router.post('/', function(req,res){
     var user = req.body;
     if(user.username == 'prof')
-        res.send(req.body);
+        res.send(jwt.encode(req.body, process.env.SECRET || 'foo'));
     else
         res.status(401).send("Bad user name and password");
 });
