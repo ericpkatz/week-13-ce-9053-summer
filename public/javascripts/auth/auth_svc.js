eaf821683bb18c27822795e47f9d47963e63c5ff
Eric Katz <ericpkatz@gmail.com>
2015-08-19 23:13:01 +0000

    add call to server

--- public/javascripts/auth/auth_svc.js
+++ public/javascripts/auth/auth_svc.js
@@ -1,13 +1,20 @@
 angular.module("my_world")
-    .factory("AuthSvc", function(){
+    .factory("AuthSvc", function($q, $http){
         var _user = {
             authenticated: function(){
                 return !!this.username;
             }
         };
         
+        function authenticate(user){
+           var dfd = $q.defer(); 
+           $http.post("/api/session", user);
+           return dfd.promise;
+        }
+        
         return {
-           user: _user 
+           user: _user,
+           authenticate: authenticate
         }
         
         
--- public/javascripts/auth/login_ctrl.js
+++ public/javascripts/auth/login_ctrl.js
@@ -7,30 +7,20 @@ angular.module("my_world")
             return;
         }
         NavSvc.setSelectedPath("/login");
-        var users = [
-            {
-                username: "Moe",
-                id: 1
-            },
-            {
-                username: "Larry",
-                id: 8
-            },
-            {
-                username: "Curly",
-                id: 9
-            }
-        ];
         
-        users.unshift({id: -1, username: "-- choose a user --"})
-        $scope.users = users;
-        $scope.user = users[0];
+        $scope.user = {};
         
-        $scope.$watch("user", function(curr, prev){
-            if(curr.id != -1){
-                AuthSvc.user.username = curr.username;
-                $location.path("/");
-            }
-        });
+        $scope.login = function(){
+            AuthSvc.authenticate($scope.user)
+                .then(
+                    function(){
+                        console.log("SUCCESS");
+                    },
+                    function(){
+                        console.log("FAILURE");
+                    }
+                );
+        }
+        
         
     });
